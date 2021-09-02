1c77b7209bfce1fd59cef7d3ff5869228d5369e5
poroping <43107292+poroping@users.noreply.github.com>
2021-09-02 15:01:51 +0000

    add IPaddress common w/ translation funcs

---
+++ models/common.go
@@ -0,0 +1,29 @@
+package models
+
+import (
+	"net"
+	"strconv"
+)
+
+type IPAddress struct {
+	Address string `json:"address"`
+	Mask    string `json:"mask"`
+	CIDR    string
+}
+
+func (ip *IPAddress) setCIDR() {
+	prefix_length, _ := net.IPMask(net.ParseIP(ip.Mask).To4()).Size()
+	cidr := ip.Address + "/" + strconv.Itoa(prefix_length)
+	ip.CIDR = cidr
+}
+
+func (ip *IPAddress) setNetmask() error {
+	ipv4Address, ipv4Net, err := net.ParseCIDR(ip.CIDR)
+	if err != nil {
+		return err
+	}
+	ip.Address = ipv4Address.String()
+	ip.Mask = ipv4Net.Mask.String()
+
+	return nil
+}
