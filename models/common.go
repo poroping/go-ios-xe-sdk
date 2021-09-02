e2dece3c22502665ea8af681fdb9711a232795a0
poroping <43107292+poroping@users.noreply.github.com>
2021-09-02 15:19:48 +0000

    omit field from json

--- models/common.go
+++ models/common.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"fmt"
 	"net"
 	"strconv"
 )
@@ -8,13 +9,18 @@ import (
 type IPAddress struct {
 	Address string `json:"address"`
 	Mask    string `json:"mask"`
-	CIDR    string
+	CIDR    string `json:"-"`
 }
 
-func (ip *IPAddress) SetCIDR() {
+func (ip *IPAddress) SetCIDR() error {
 	prefix_length, _ := net.IPMask(net.ParseIP(ip.Mask).To4()).Size()
+	if ip.Mask != "0.0.0.0" && prefix_length == 0 {
+		return fmt.Errorf("expected %s to be a valid IPv4 netmask", ip.Mask)
+	}
 	cidr := ip.Address + "/" + strconv.Itoa(prefix_length)
 	ip.CIDR = cidr
+
+	return nil
 }
 
 func (ip *IPAddress) SetNetmask() error {
@@ -23,7 +29,7 @@ func (ip *IPAddress) SetNetmask() error {
 		return err
 	}
 	ip.Address = ipv4Address.String()
-	ip.Mask = ipv4Net.Mask.String()
+	ip.Mask = fmt.Sprintf("%d.%d.%d.%d", ipv4Net.Mask[0], ipv4Net.Mask[1], ipv4Net.Mask[2], ipv4Net.Mask[3])
 
 	return nil
 }
