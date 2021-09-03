967947a21c7ca6353e97f0db5bd05b387f6da652
Freyr Guðjónsson <ze@anime.is>
2021-09-03 10:11:14 +0000

    Move paths to common

--- models/common.go
+++ models/common.go
@@ -33,3 +33,21 @@ func (ip *IPAddress) SetNetmask() error {
 
 	return nil
 }
+
+
+const BaseURI = "restconf/data/Cisco-IOS-XE-native:native"
+var (
+	BgpBaseURI = fmt.Sprintf("%s/%s", BaseURI, "router/Cisco-IOS-XE-bgp:bgp")
+)
+
+func GetBaseURI() string {
+	return BaseURI
+}
+
+func GetBgpURI(asn string) string {
+	return fmt.Sprintf("%s=%s", BgpBaseURI, asn)
+}
+
+func GetBgpNeighborURI(asn, neighbor string) string {
+	return fmt.Sprintf("%s/neighbor=%s", string(GetBgpURI(asn)), neighbor)
+}
