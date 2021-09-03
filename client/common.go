72371dc92751031bcadae21bd8830712c193f819
poroping <43107292+poroping@users.noreply.github.com>
2021-09-03 13:14:34 +0000

    add extra uri func

--- client/common.go
+++ client/common.go
@@ -5,6 +5,7 @@ import (
 )
 
 const BaseURI = "restconf/data/Cisco-IOS-XE-native:native"
+
 var (
 	BgpBaseURI = fmt.Sprintf("%s/%s", BaseURI, "router/Cisco-IOS-XE-bgp:bgp")
 )
@@ -19,4 +20,12 @@ func GetBgpURI(asn string) string {
 
 func GetBgpNeighborURI(asn, neighbor string) string {
 	return fmt.Sprintf("%s/neighbor=%s", string(GetBgpURI(asn)), neighbor)
+}
+
+func GetBgpNeighborConfigURI(asn, neighbor string, vrf *string) string {
+	if vrf == nil {
+		noVrf := "no-vrf"
+		vrf = &noVrf
+	}
+	return fmt.Sprintf("%s/address-family/%s/ipv4/unicast/ipv4-unicast/neighbor=%s", string(GetBgpURI(asn)), &vrf, neighbor)
 }
